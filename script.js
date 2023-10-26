4b7b1db57d56f7757a8faf492f0fdc8395aa3362
Aepex25 <angelestevezsalinas@gmail.com>
2023-10-26 11:47:06 -0400

    basic structure and board size funtion

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello");
+"use strict";
+
+// Elements
+const board = document.querySelector(".board");
+
+// Board Size
+
+function boardSize(size) {
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+}
+
+boardSize(32);
