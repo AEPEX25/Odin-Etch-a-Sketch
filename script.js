7674d81d9ef9031030a4351e6c4f1f60fd298b8d
Aepex25 <angelestevezsalinas@gmail.com>
2023-10-30 11:18:20 -0400

    append divs to board

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ const board = document.querySelector(".board");
 function boardSize(size) {
   board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  let amount = size * size;
+
+  for (let i = 0; i < amount; i++) {
+    let square = document.createElement("div");
+    square.style.backgroundColor = "white";
+    board.appendChild(square);
+  }
 }
 
-boardSize(32);
+boardSize(16);
