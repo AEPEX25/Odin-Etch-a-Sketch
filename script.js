31780b76618922cecf007fc966313fc77758b0d7
Aepex25 <angelestevezsalinas@gmail.com>
2023-11-07 08:33:08 -0500

    board size button

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 // Elements
 const board = document.querySelector(".board");
+const btnBoardSize = document.querySelector(".btn-board-size");
 
 // Board Size
 
 function boardSize(size) {
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
   board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
@@ -20,3 +23,12 @@ function boardSize(size) {
 }
 
 boardSize(16);
+
+btnBoardSize.addEventListener("click", function () {
+  let size = prompt("What board size do you want?");
+  if (size < 2 || size > 100) {
+    alert("Max square number surpassed");
+  } else {
+    boardSize(size);
+  }
+});
